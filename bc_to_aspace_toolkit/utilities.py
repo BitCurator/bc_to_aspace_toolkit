769a239e3d02ce096a436e1c107f8edf930c8ba0
Kam Woods <kamwoods@gmail.com>
2019-01-05 16:45:06 -0500

    Resource support updates

--- bc_to_aspace_toolkit/utilities.py
+++ bc_to_aspace_toolkit/utilities.py
@@ -15,10 +15,10 @@ import pkg_resources
 
 def get_json_template(template_name):
     resource_package = "bc_to_aspace_toolkit"  # Exact package name here
-    resource_path = '/'.join(('json_templates', 'create_archival_objects.json'))  # Do not use os.path.join()
+    resource_path = '/'.join(('json_templates', template_name))  # Do not use os.path.join()
 
     # String alternative (reference only, we'll use the file stream)
-    template = pkg_resources.resource_string(resource_package, resource_path)
+    #template = pkg_resources.resource_string(resource_package, resource_path)
     # A file-like stream:
     template = pkg_resources.resource_stream(resource_package, resource_path)
-    return(template)
+    return template
