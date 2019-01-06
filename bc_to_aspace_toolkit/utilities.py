3231cc77400484cf631061ad69acbc3790b59e82
Kam Woods <kamwoods@gmail.com>
2019-01-06 00:09:13 -0500

    Minor response mods

--- bc_to_aspace_toolkit/utilities.py
+++ bc_to_aspace_toolkit/utilities.py
@@ -16,9 +16,9 @@ import pkg_resources
 def ask_user(question):
     while "Please enter y or n":
         if sys.version_info[0] < 3:
-            reply = str(raw_input(question+' (y/n): ')).lower().strip()
+            reply = str(raw_input(question+' [y/N]: ')).lower().strip()
         else:
-            reply = str(input(question+' (y/n): ')).lower().strip()
+            reply = str(input(question+' [y/N]: ')).lower().strip()
         if reply[:1] == 'y':
             return True
         if reply[:1] == 'n':
